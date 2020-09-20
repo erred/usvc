e2dabd085423416751afcda4a1ae5984f1959779
Sean Liao <seankhliao@gmail.com>
2020-09-20 18:35:53 +0200

    add grpc support

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/rs/zerolog v1.18.0
 	go.opentelemetry.io/otel v0.11.0
 	go.opentelemetry.io/otel/exporters/metric/prometheus v0.11.0
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	google.golang.org/grpc v1.31.0
 )
--- usvc.go
+++ usvc.go
@@ -6,10 +6,12 @@ import (
 	"errors"
 	"flag"
 	"io"
+	"net"
 	"net/http"
 	"net/http/pprof"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -18,6 +20,9 @@ import (
 	"go.opentelemetry.io/otel/api/metric"
 	"go.opentelemetry.io/otel/api/unit"
 	"go.opentelemetry.io/otel/exporters/metric/prometheus"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type FlagRegisterer interface {
@@ -26,7 +31,8 @@ type FlagRegisterer interface {
 
 // Conf holds configs for creating a http.Server
 type Conf struct {
-	Addr        string
+	HTTPAddr    string
+	GRPCAddr    string
 	TLSCertFile string
 	TLSKeyFile  string
 	LogLevel    string
@@ -50,7 +56,8 @@ func DefaultConf(frs ...FlagRegisterer) Conf {
 
 // RegisterFlags adds flags to flagset
 func (c *Conf) RegisterFlags(fs *flag.FlagSet) {
-	fs.StringVar(&c.Addr, "addr", ":8080", "listen addr")
+	fs.StringVar(&c.HTTPAddr, "http.addr", ":8080", "listen addr for http")
+	fs.StringVar(&c.GRPCAddr, "grpc.addr", ":8080", "listen addr for grpc")
 	fs.StringVar(&c.TLSCertFile, "tls.crt", "", "tls cert file")
 	fs.StringVar(&c.TLSKeyFile, "tls.key", "", "tls key file")
 	fs.StringVar(&c.LogLevel, "log.level", "", "logging level: debug, info, warn, error")
@@ -74,56 +81,101 @@ func (c Conf) Logger() zerolog.Logger {
 	return zerolog.New(out).Level(lvl).With().Timestamp().Logger()
 }
 
-// RunServer is a convenience function to run the server without modification
-func (c Conf) RunServer(ctx context.Context, h http.Handler, log zerolog.Logger) error {
-	_, run, err := c.Server(h, log)
-	if err != nil {
-		return err
-	}
-	err = run(ctx)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+type Runner func(context.Context) error
 
-// Server returns a http.Server if modification is needed,
-// a run function taking a cancellation/shutdown context
-// and returns any errors
-//
-// Expects a global Meter with name os.Args[0]
-//
-// Inserts /debug/pprof/, /health, and /metrics endpoints
-// and wraps all handlers with CORS and request logging
-func (c Conf) Server(h http.Handler, log zerolog.Logger) (*http.Server, func(context.Context) error, error) {
+func (c Conf) Server(m *http.ServeMux) (*http.Server, *grpc.Server, Runner, error) {
 	latency := metric.Must(global.Meter(os.Args[0])).NewInt64ValueRecorder(
 		"request_latency_ms",
 		metric.WithDescription("http request serve latency"),
 		metric.WithUnit(unit.Milliseconds),
 	)
 
-	if m, ok := h.(*http.ServeMux); ok {
-		m.HandleFunc("/debug/pprof/", pprof.Index)
-		m.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-		m.HandleFunc("/debug/pprof/profile", pprof.Profile)
-		m.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-		m.HandleFunc("/debug/pprof/trace", pprof.Trace)
-		m.Handle("/health", healthOK)
-		promExporter, _ := prometheus.InstallNewPipeline(prometheus.Config{
-			DefaultHistogramBoundaries: []float64{1, 5, 10, 50, 100},
-		})
-		m.Handle("/metrics", promExporter)
+	if m == nil {
+		m = http.NewServeMux()
 	}
 
-	h = logMid(h, log, latency)
+	m.HandleFunc("/debug/pprof/", pprof.Index)
+	m.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	m.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	m.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	m.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	m.Handle("/health", healthOK)
+	promExporter, _ := prometheus.InstallNewPipeline(prometheus.Config{
+		DefaultHistogramBoundaries: []float64{1, 5, 10, 50, 100},
+	})
+	m.Handle("/metrics", promExporter)
+
+	// http
+	h := httpMid(m, c.Logger(), latency)
 	h = corsAllowAll(h)
 
+	// grpc
+	var opts []grpc.ServerOption
+	if c.TLSKeyFile != "" {
+		creds, err := credentials.NewServerTLSFromFile(c.TLSCertFile, c.TLSKeyFile)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		opts = append(opts, grpc.Creds(creds))
+	}
+	opts = append(opts, grpcMid(c.Logger(), latency))
+	grpcServer := grpc.NewServer(opts...)
+
+	// share
+	if c.GRPCAddr == c.HTTPAddr {
+		httpServer, run, err := c.sharedServer(h, grpcServer)
+		return httpServer, grpcServer, run, err
+	}
+
+	// separate
+	grpcRun := func(ctx context.Context) error {
+		go func() {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+			select {
+			case <-c:
+			case <-ctx.Done():
+			}
+			grpcServer.GracefulStop()
+		}()
+
+		lis, err := net.Listen("tcp", c.GRPCAddr)
+		if err != nil {
+			return err
+		}
+		return grpcServer.Serve(lis)
+	}
+	httpServer, httpRun, err := c.sharedServer(h, nil)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	run := func(ctx context.Context) error {
+		group, ctx := errgroup.WithContext(ctx)
+		group.Go(func() error { return httpRun(ctx) })
+		group.Go(func() error { return grpcRun(ctx) })
+		return group.Wait()
+	}
+	return httpServer, grpcServer, run, nil
+
+}
+
+func (c Conf) sharedServer(h http.Handler, grpcServer *grpc.Server) (*http.Server, Runner, error) {
+	var dispatch http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	})
+	if grpcServer == nil {
+		dispatch = h
+	}
+
 	srv := &http.Server{
-		Addr:              c.Addr,
-		Handler:           h,
-		ReadTimeout:       5 * time.Second,
+		Addr:              c.HTTPAddr,
+		Handler:           dispatch,
 		ReadHeaderTimeout: 10 * time.Second,
-		WriteTimeout:      5 * time.Second,
 		IdleTimeout:       60 * time.Second,
 		MaxHeaderBytes:    1 << 20,
 		TLSConfig: &tls.Config{
@@ -141,12 +193,21 @@ func (c Conf) Server(h http.Handler, log zerolog.Logger) (*http.Server, func(con
 			case <-c:
 			case <-ctx.Done():
 			}
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			go func() {
-				<-c
-				cancel()
-			}()
-			se <- srv.Shutdown(ctx)
+
+			// call shutdown and wait for both
+			gc := make(chan struct{})
+			if grpcServer != nil {
+				go func() {
+					grpcServer.GracefulStop()
+					close(gc)
+				}()
+			}
+			err := srv.Shutdown(context.Background())
+			if grpcServer != nil {
+				<-gc
+			}
+
+			se <- err
 		}()
 
 		var err error
@@ -162,59 +223,4 @@ func (c Conf) Server(h http.Handler, log zerolog.Logger) (*http.Server, func(con
 	}
 
 	return srv, run, nil
-}
-
-func logMid(h http.Handler, log zerolog.Logger, latency metric.Int64ValueRecorder) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.URL.Path {
-		case "/metrics", "/health":
-			h.ServeHTTP(w, r)
-			return
-		}
-
-		t := time.Now()
-		remote := r.Header.Get("x-forwarded-for")
-		if remote == "" {
-			remote = r.RemoteAddr
-		}
-		ua := r.Header.Get("user-agent")
-
-		defer func() {
-			d := time.Since(t)
-			latency.Record(r.Context(), d.Milliseconds())
-			log.Debug().
-				Str("src", remote).
-				Str("url", r.URL.String()).
-				Str("user-agent", ua).
-				Dur("dur", d).
-				Msg("served")
-		}()
-
-		h.ServeHTTP(w, r)
-	})
-}
-
-var healthOK = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-})
-
-func corsAllowAll(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodOptions:
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
-			w.Header().Set("Access-Control-Max-Age", "86400")
-			w.WriteHeader(http.StatusNoContent)
-			return
-		case http.MethodGet, http.MethodPost:
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
-			w.Header().Set("Access-Control-Max-Age", "86400")
-			h.ServeHTTP(w, r)
-		default:
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-	})
 }
