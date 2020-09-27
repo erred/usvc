3b5130ef4f5e8d8376a66caca7e005584852f801
Sean Liao <seankhliao@gmail.com>
2020-09-27 18:11:33 +0200

    rework

---
+++ tracer.go
@@ -0,0 +1,42 @@
+package usvc
+
+import (
+	"flag"
+
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/propagation"
+	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+type TracerOpts struct {
+	CollectorEndpoint string
+}
+
+func (o *TracerOpts) Flags(fs *flag.FlagSet) {
+	fs.StringVar(&o.CollectorEndpoint, "trace.collector", "http://jaeger:14268/api/traces?format=jaeger.thrift", "jaeger collector endpoint")
+}
+
+// Tracer installs a global tracer
+func (o TracerOpts) Tracer() (shutdown func() error, err error) {
+	flush, err := jaeger.InstallNewPipeline(
+		jaeger.WithCollectorEndpoint(
+			o.CollectorEndpoint,
+			jaeger.WithCollectorEndpointOptionFromEnv(),
+		),
+		jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.NeverSample()}),
+		// jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+	)
+	shutdown = func() error {
+		flush()
+		return nil
+	}
+
+	b3 := b3.B3{}
+	global.SetPropagators(propagation.New(
+		propagation.WithExtractors(b3),
+		propagation.WithInjectors(b3),
+	))
+	return
+}
--- usvc.go
+++ usvc.go
@@ -2,123 +2,324 @@ package usvc
 
 import (
 	"context"
-	"crypto/tls"
+	"errors"
 	"flag"
+	"log"
 	"net"
 	"net/http"
-	"net/http/pprof"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 
-	otelgrpc "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc"
-	otelhttp "go.opentelemetry.io/contrib/instrumentation/net/http"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/rs/zerolog"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/trace"
+	"go.seankhliao.com/apis/saver/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
 )
 
-// Service only mandates a name,
-// everythign else is optional
 type Service interface {
-	// Flagger is for registering flags
-	Flag(fs *flag.FlagSet)
-	// Register is the time to
-	// get the logger, tracer, metric;
-	// add routes, services;
-	// start background services
-	Register(c *Components) error
-	// Shutdown an optional interface to implement
-	// to be called in the graceful shutdown sequence
-	Shutdown(ctx context.Context) error
+	Flags(fs *flag.FlagSet)
+	Setup(ctx context.Context, usvc *USVC) error
 }
 
-func Run(ctx context.Context, name string, server Service, grpcsvc bool) {
-	fs := flag.NewFlagSet(name, flag.ExitOnError)
-	metricAddr := fs.String("addr.metric", ":8000", "metric address")
-	serviceAddr := fs.String("addr", ":8080", "service address")
-	tlsCert := fs.String("tls.crt", "", "tls cert file")
-	tlsKey := fs.String("tls.key", "", "tls key file")
-	caCrt := fs.String("ca.crt", "", "ca crt for client connections")
-	logLevel := fs.String("log.lvl", "trace", "log level: trace, debug, info, error")
-	logFormat := fs.String("log.fmt", "json", "log format: logfmt, json")
-	saverURL := fs.String("saver", "", "host:port for go.seankhliao.com/saver")
-	server.Flag(fs)
-	fs.Parse(os.Args[1:])
+type USVC struct {
+	// external
+	Logger zerolog.Logger
+	// tracer is global
+	// Tracer trace.Tracer
+	// metrics is global
 
-	c, err := NewComponents(name, *logLevel, *logFormat, *caCrt, *saverURL)
+	ServiceMux    *http.ServeMux
+	MetricMux     *http.ServeMux
+	Live          HealthProbe
+	Ready         HealthProbe
+	ServiceServer *http.Server
+	MetricServer  *http.Server
+	GRPCServer    *grpc.Server
+
+	// internal
+	log    zerolog.Logger
+	tracer trace.Tracer
+}
+
+// Exec runs the server
+// returning an os.Exit code
+//
+// Globals: prometheus metrics, otel tracer
+func Exec(ctx context.Context, svc Service, args []string) int {
+	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		sigc := make(chan os.Signal, 1)
+		signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+		<-sigc
+		cancel()
+	}()
+
+	var (
+		serviceAddr string
+		metricAddr  string
+		loggerOpts  LoggerOpts
+		metricOpts  MetricOpts
+		tracerOpts  TracerOpts
+		saverOpts   SaverOpts
+		tlsOpts     TLSOpts
+	)
+
+	fs := flag.NewFlagSet(args[0], flag.ExitOnError)
+	fs.StringVar(&serviceAddr, "addr", ":8080", "service listen address")
+	fs.StringVar(&metricAddr, "addr.metric", ":8000", "metric listen address")
+	loggerOpts.Flags(fs)
+	metricOpts.Flags(fs)
+	tracerOpts.Flags(fs)
+	saverOpts.Flag(fs)
+	tlsOpts.Flags(fs)
+	svc.Flags(fs)
+	fs.Parse(args[1:])
+
+	usvc := USVC{
+		Logger:     loggerOpts.Logger(true),
+		ServiceMux: http.NewServeMux(),
+		MetricMux:  http.NewServeMux(),
+	}
+
+	usvc.log = usvc.Logger.With().Str("module", "usvc").Logger()
+	usvc.Live, usvc.Ready = metricOpts.Metrics(usvc.MetricMux)
+
+	traceShut, err := tracerOpts.Tracer()
 	if err != nil {
-		c.Log.Error().Err(err).Msg("setup components")
-		return
+		usvc.log.Error().Err(err).Msg("init tracer")
+		return 1
 	}
+	defer traceShut()
+	usvc.tracer = global.Tracer("usvc")
 
-	tlsConf, gopts, err := tlsSetup(*tlsCert, *tlsKey)
+	tlsConf, err := tlsOpts.Config()
 	if err != nil {
-		c.Log.Error().Err(err).Str("crt", *tlsCert).Str("key", *tlsKey).Msg("setup tls")
-		return
+		usvc.log.Error().Err(err).Msg("init tls")
+		return 1
 	}
 
-	// metrics endpoint
-	mmux := http.NewServeMux() // metrics http
-	mmux.Handle("/metrics", c.prom)
-	mmux.HandleFunc("/health", okhandler)
-	pprofHandlers(mmux)
-	msrv := httpServer(*metricAddr, mmux, nil)
-
-	// grpc server endpoint
-	gopts = append(gopts, grpc.ChainUnaryInterceptor(otelgrpc.UnaryServerInterceptor(c.Tracer), c.unaryLog))
-	c.GRPC = grpc.NewServer(gopts...)
-
-	// http server endpoint
-	c.HTTP = http.NewServeMux() // service http
-	hhandler := otelhttp.NewHandler(c.httpLog(corsAllowAll(c.HTTP)), name, otelhttp.WithMessageEvents(otelhttp.ReadEvents, otelhttp.WriteEvents))
-	hsrv := httpServer(*serviceAddr, hhandler, tlsConf)
-
-	// register other services
-	err = server.Register(c)
+	var do grpc.DialOption
+	if tlsConf.RootCAs != nil {
+		do = grpc.WithTransportCredentials(credentials.NewTLS(tlsConf))
+	} else {
+		do = grpc.WithInsecure()
+	}
+	saverClient, saverShut, err := saverOpts.Saver(do)
 	if err != nil {
-		c.Log.Error().Err(err).Msg("register service")
-		return
+		usvc.log.Error().Err(err).Msg("init saver")
+		return 1
+	}
+	defer saverShut()
+
+	// middleware
+	latency := promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "usvc_service_latency_s",
+	}, []string{"proto"})
+
+	var sos []grpc.ServerOption
+	if len(tlsConf.Certificates) > 0 {
+		sos = append(sos, grpc.Creds(credentials.NewTLS(tlsConf)))
+	}
+	sos = append(sos, grpc.ChainUnaryInterceptor(
+		otelgrpc.UnaryServerInterceptor(global.Tracer("usvc")),
+		unaryLogMiddleware(
+			usvc.tracer,
+			usvc.log,
+			latency,
+		),
+	))
+	usvc.GRPCServer = grpc.NewServer(sos...)
+
+	handler := otelhttp.NewHandler(
+		httpLogMiddleWare(
+			usvc.tracer,
+			usvc.log,
+			latency,
+			saverClient,
+			corsAllowAll(usvc.ServiceMux),
+		),
+		"usvc",
+		otelhttp.WithMessageEvents(otelhttp.ReadEvents, otelhttp.WriteEvents),
+	)
+	// handler := usvc.ServiceMux
+	// _ = saverClient
+	usvc.ServiceServer = &http.Server{
+		Addr:              serviceAddr,
+		Handler:           handler,
+		ReadHeaderTimeout: 10 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+		TLSConfig:         tlsConf,
+		ErrorLog:          log.New(usvc.Logger.With().Str("module", "http.service").Logger(), "", 0),
 	}
 
-	// run
-	svc := "http"
-	if grpcsvc {
-		svc = "grpc"
+	usvc.MetricServer = &http.Server{
+		Addr:              metricAddr,
+		Handler:           usvc.MetricMux,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+		ErrorLog:          log.New(usvc.Logger.With().Str("module", "http.metric").Logger(), "", 0),
 	}
-	c.Log.Info().Str(svc, *serviceAddr).Bool("tls", tlsConf != nil).Str("metric", *metricAddr).Msg("starting server")
-	errc, done := startServers(ctx, msrv, hsrv, c.GRPC, tlsConf, grpcsvc)
-	<-done
-	shutdownServers(errc, server, msrv, hsrv, c.GRPC, grpcsvc)
+
+	err = svc.Setup(ctx, &usvc)
+	if err != nil {
+		usvc.log.Error().Err(err).Msg("service setup")
+		return 1
+	}
+
+	errs := usvc.run(ctx, cancel)
+	if err != nil {
+		usvc.log.Error().Errs("errs", errs).Msg("service run")
+		return 1
+	}
+	usvc.log.Info().Msg("service shutdown cleanly")
+	return 0
+}
+
+func (u *USVC) run(ctx context.Context, cancel func()) []error {
+	errc := make(chan error)
+	g := len(u.GRPCServer.GetServiceInfo()) != 0
+	if g {
+		go func() {
+			u.log.Info().Str("addr", u.ServiceServer.Addr).Msg("starting service grpc endpoint")
+			lis, err := net.Listen("tcp", u.ServiceServer.Addr)
+			if err != nil {
+				cancel()
+				errc <- err
+				return
+			}
+			err = u.GRPCServer.Serve(lis)
+			cancel()
+			errc <- err
+		}()
+	} else {
+		go func() {
+			u.log.Info().Str("addr", u.ServiceServer.Addr).Msg("starting service http endpoint")
+			var err error
+			if u.ServiceServer.TLSConfig != nil && len(u.ServiceServer.TLSConfig.Certificates) > 0 {
+				err = u.ServiceServer.ListenAndServeTLS("", "")
+			} else {
+				err = u.ServiceServer.ListenAndServe()
+			}
+			cancel()
+			errc <- err
+		}()
+	}
+
+	go func() {
+		u.log.Info().Str("addr", u.MetricServer.Addr).Msg("starting metrics endpoint")
+		err := u.MetricServer.ListenAndServe()
+		cancel()
+		errc <- err
+	}()
+
+	<-ctx.Done()
+	u.log.Info().Msg("starting shutdown sequence")
+
+	sdctx := context.Background()
+	go func() {
+		errc <- u.MetricServer.Shutdown(sdctx)
+	}()
+	if g {
+		go func() {
+			u.GRPCServer.GracefulStop()
+			errc <- nil
+		}()
+	} else {
+		go func() {
+			errc <- u.ServiceServer.Shutdown(sdctx)
+		}()
+	}
 
 	var errs []error
-	for err := range errc {
-		if err != nil {
-			errs = append(errs, err)
+	for i := 0; i < 4; i++ {
+		e := <-errc
+		if errors.Is(e, http.ErrServerClosed) {
+			// skip
+		} else if e != nil {
+			errs = append(errs, e)
 		}
 	}
-	c.Log.Error().Errs("shutdown", errs).Msg("exit")
+	return errs
 }
 
-func tlsSetup(tlsCert, tlsKey string) (*tls.Config, []grpc.ServerOption, error) {
-	if tlsCert != "" && tlsKey != "" {
-		cert, err := tls.LoadX509KeyPair(tlsCert, tlsKey)
-		if err != nil {
-			return nil, nil, err
-		}
-		tlsConf := &tls.Config{
-			Certificates: []tls.Certificate{cert},
-			MinVersion:   tls.VersionTLS13,
-		}
-		gopts := []grpc.ServerOption{grpc.Creds(credentials.NewTLS(tlsConf))}
-		return tlsConf, gopts, nil
+func httpLogMiddleWare(tracer trace.Tracer, log zerolog.Logger, latency *prometheus.HistogramVec, saverClient saver.SaverClient, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		defer func() {
+			d := time.Since(t)
+			latency.With(prometheus.Labels{"proto": r.Proto}).Observe(d.Seconds())
+
+			remote := r.Header.Get("x-forwarded-for")
+			if remote == "" {
+				remote = r.RemoteAddr
+			}
+			log.Trace().
+				Str("src", remote).
+				Str("url", r.URL.String()).
+				Str("user-agent", r.UserAgent()).
+				Dur("dur", d).
+				Msg("served")
+
+			_, err := saverClient.HTTP(context.Background(), &saver.HTTPRequest{
+				HttpRemote: &saver.HTTPRemote{
+					Timestamp: t.Format(time.RFC3339),
+					Remote:    remote,
+					UserAgent: r.UserAgent(),
+					Referrer:  r.Referer(),
+				},
+				Method: r.Method,
+				Domain: r.Host,
+				Path:   r.URL.Path,
+			})
+			if err != nil {
+				log.Error().Err(err).Msg("send to saver")
+			}
+		}()
+
+		ctx, span := tracer.Start(r.Context(), "usvc")
+		defer span.End()
+
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func unaryLogMiddleware(tracer trace.Tracer, log zerolog.Logger, latency *prometheus.HistogramVec) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		t := time.Now()
+		defer func() {
+			d := time.Since(t)
+			latency.With(prometheus.Labels{"proto": "grpc"}).Observe(d.Seconds())
+
+			var pa string
+			p, ok := peer.FromContext(ctx)
+			if ok {
+				pa = p.Addr.String()
+			}
+
+			log.Trace().
+				Str("src", pa).
+				Str("method", info.FullMethod).
+				Dur("dur", d).
+				Msg("served")
+		}()
+
+		ctx, span := tracer.Start(ctx, "usvc")
+		defer span.End()
+
+		return handler(ctx, req)
 	}
-	return nil, nil, nil
-}
-
-func okhandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 }
 
 func corsAllowAll(h http.Handler) http.Handler {
@@ -140,103 +341,4 @@ func corsAllowAll(h http.Handler) http.Handler {
 			return
 		}
 	})
-}
-
-func startServers(ctx context.Context, msrv, hsrv *http.Server, gsrv *grpc.Server, tlsConf *tls.Config, grpcsvc bool) (chan error, <-chan struct{}) {
-	errc := make(chan error)
-	ctx, cancel := context.WithCancel(ctx)
-	go func() {
-		sigc := make(chan os.Signal, 1)
-		signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
-		<-sigc
-		cancel()
-	}()
-	go func() {
-		err := msrv.ListenAndServe()
-		cancel()
-		errc <- err
-	}()
-	if grpcsvc {
-		go func() {
-			lis, err := net.Listen("tcp", hsrv.Addr)
-			if err != nil {
-				cancel()
-				errc <- err
-				return
-			}
-			err = gsrv.Serve(lis)
-			cancel()
-			errc <- err
-		}()
-	} else {
-		go func() {
-			var err error
-			if tlsConf == nil {
-				err = hsrv.ListenAndServe()
-			} else {
-				err = hsrv.ListenAndServeTLS("", "")
-			}
-			cancel()
-			errc <- err
-		}()
-	}
-	return errc, ctx.Done()
-}
-
-func shutdownServers(errc chan error, server Service, msrv, hsrv *http.Server, gsrv *grpc.Server, grpcsvc bool) {
-	sdctx := context.Background()
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		errc <- server.Shutdown(sdctx)
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		errc <- msrv.Shutdown(sdctx)
-	}()
-	if grpcsvc {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			gsrv.GracefulStop()
-		}()
-	} else {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			errc <- hsrv.Shutdown(sdctx)
-		}()
-	}
-
-	go func() {
-		wg.Wait()
-		close(errc)
-	}()
-}
-
-func pprofHandlers(mmux *http.ServeMux) {
-	mmux.HandleFunc("/debug/pprof/", pprof.Index)
-	mmux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mmux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mmux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mmux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	mmux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	mmux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	mmux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
-	mmux.Handle("/debug/pprof/block", pprof.Handler("block"))
-}
-
-func httpServer(addr string, h http.Handler, tlsConf *tls.Config) *http.Server {
-	return &http.Server{
-		Addr:    addr,
-		Handler: h,
-		// ReadTimeout:       10 * time.Second,
-		ReadHeaderTimeout: 10 * time.Second,
-		// WriteTimeout:      10 * time.Second,
-		// IdleTimeout:       60 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-		TLSConfig:      tlsConf,
-	}
 }
