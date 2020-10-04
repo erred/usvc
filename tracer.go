9318d4fb95c19dc2bb796afd77fc45d3aa497aee
Sean Liao <seankhliao@gmail.com>
2020-10-04 11:43:35 +0200

    disable trace by default

--- tracer.go
+++ tracer.go
@@ -7,29 +7,36 @@ import (
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/propagation"
 	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
 type TracerOpts struct {
+	Enabled           bool
 	CollectorEndpoint string
 }
 
 func (o *TracerOpts) Flags(fs *flag.FlagSet) {
+	fs.BoolVar(&o.Enabled, "trace", false, "enable tracing")
 	fs.StringVar(&o.CollectorEndpoint, "trace.collector", "http://jaeger:14268/api/traces?format=jaeger.thrift", "jaeger collector endpoint")
 }
 
 // Tracer installs a global tracer
 func (o TracerOpts) Tracer() (shutdown func() error, err error) {
+	var opts []jaeger.Option
+	opts = append(opts, jaeger.WithProcess(jaeger.Process{
+		ServiceName: name,
+	}))
+	opts = append(opts, jaeger.WithProcessFromEnv())
+	if !o.Enabled {
+		opts = append(opts, jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.NeverSample()}))
+	}
+
 	flush, err := jaeger.InstallNewPipeline(
 		jaeger.WithCollectorEndpoint(
 			o.CollectorEndpoint,
 			jaeger.WithCollectorEndpointOptionFromEnv(),
 		),
-		jaeger.WithProcess(jaeger.Process{
-			ServiceName: name,
-		}),
-		jaeger.WithProcessFromEnv(),
-		// jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.NeverSample()}),
-		// jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+		opts...,
 	)
 	shutdown = func() error {
 		flush()
